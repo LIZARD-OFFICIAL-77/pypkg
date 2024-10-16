2396c0c78b998981fdecabd6e657fe15f5c7adf8
LIZARD-OFFICIAL-77 <lizard.github.77@gmail.com>
2024-10-16 19:10:56 +0200

    Initial Commit

---
+++ build.py
@@ -0,0 +1,46 @@
+import tarfile
+import random
+import atexit
+import shutil
+import yaml
+import os
+
+class JsonDotDesktopBuilder:
+    def __init__(self,json: dict):
+        self.build = "[Desktop Entry]\n"
+        for k,v in json.items():
+            self.build = f"{k}={v}\n"
+
+class AppBuilder:
+    def __init__(self,name):
+        """Class for building pypkg apps.
+
+        Args:
+            name (str): App name
+        """
+        self.name = name
+
+        
+        self.tempfolder = "pypkgbuild"+str(random.randint(11111,99999)) # Create temp folder because tarfile needs it.
+        os.mkdir(self.tempfile)
+        atexit.register(self.deltemp)
+
+        self.approot = tarfile.open(f"{self.tempfolder}/approot.tar.gz","w:")
+        self.build = tarfile.open(f"{self.tempfolder}/build.tar.gz","w:")
+
+        self.dotdesktop = {}
+    
+    def __setitem__(self,key,value):
+        self.dotdesktop[key] = value
+
+    def deltemp(self):
+        shutil.rmtree(self.tempfile)
+    
+    def add(self,path):
+        """Add file or folder to the app directory.
+
+        Args:
+            path (str): Location of the file or folder.
+        """
+        self.approot.add(path)
+    
---
+++ virtualenvironment.py
@@ -0,0 +1,24 @@
+import pathlib
+import venv
+import os
+
+def delenv(name="throwerrror"):
+    os.system(f"rm -rf ~/.pypkg/appenvs/{name}")
+
+class AppEnvironment:
+    def __init__(self,name):
+        self.name = name
+        self.path = f"{os.path.expanduser("~")}/.pypkg/appenvs/{self.name}"
+        if not pathlib.Path(self.path).is_dir():
+            venv.create(f"{self.path}/venv")
+            os.system(f"{self.path}/venv/bin/python ensurepip --defualt-pip")
+            os.mkdir(self.path)
+    
+    def getpath(self,path):
+        return self.path + path
+    
+    def python(self,command):
+        os.system(f"{self.path}/venv/bin/python {command}")
+
+    def pip(self,command):
+        os.system(f"{self.path}/venv/bin/pip {command}")
