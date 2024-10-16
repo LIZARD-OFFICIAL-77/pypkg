7eb2b4457683b7031f5a32ef41057f89a7fdca61
LIZARD-OFFICIAL-77 <lizard.github.77@gmail.com>
2024-10-16 23:33:38 +0200

    Build system finished (?)

--- build.py
+++ build.py
@@ -2,44 +2,67 @@ import tarfile
 import random
 import atexit
 import shutil
+import json
 import os
 
 class JsonDotDesktopBuilder:
     def __init__(self,json: dict):
         self.build = "[Desktop Entry]\n"
         for k,v in json.items():
-            self.build = f"{k}={v}\n"
+                self.build = f"{k}={v}\n"
 
 class AppBuilder:
-    def __init__(self,name):
+    def __init__(self):
         """Class for building pypkg apps.
+        """
+
+        # Setup Temporary build folder
+        self.tempfolder = "pypkgbuildtmp"+str(random.randint(11111,99999)) 
+        os.mkdir(self.tempfolder)
+
+        self.build = tarfile.open(f"build.tar.gz","w:")
+
+        self.config = {"dotdesktop":{}}
+
+        atexit.register(self.rmtemp) # make sure to get rid of those
+        atexit.register(self.build.close)
+
+    def __setitem__(self,key,value):
+        self.config[key] = value
+    def __getitem__(self,key):
+        return self.config[key]
+
+    def rmtemp(self):
+        shutil.rmtree(self.tempfolder)
+    
+    def set_exec(self,executablepath):
+        """Set python file that represents the app.
+
+        To help precisely set the executable, %APPROOT represents the app directory. (e.g. $APPROOT/main.py)
 
         Args:
-            name (str): App name
+            executablepath (str): Path of the python file that should be launched when app is launched.
         """
-        self.name = name
+        self.config["executable"] = executablepath
+    def set_icon(self,icon: str):
+        """Set application icon.
 
-        
-        self.tempfolder = "pypkgbuild"+str(random.randint(11111,99999)) # Create temp folder because tarfile needs it.
-        os.mkdir(self.tempfile)
-        atexit.register(self.deltemp)
-
-        self.approot = tarfile.open(f"{self.tempfolder}/approot.tar.gz","w:")
-        self.build = tarfile.open(f"{self.tempfolder}/build.tar.gz","w:")
-
-        self.dotdesktop = {}
-    
-    def __setitem__(self,key,value):
-        self.dotdesktop[key] = value
-
-    def deltemp(self):
-        shutil.rmtree(self.tempfile)
-    
+        Args:
+            icon (str): Icon path.
+        """
+        self.build.add(
+            icon,arcname="icon."+icon.split(".")[-1] # rename icon to just "icon" in build archive to keep things simple
+        )
     def add(self,path):
-        """Add file or folder to the app directory.
+        """Add file or folder to the $APPROOT directory.
 
         Args:
             path (str): Location of the file or folder.
         """
-        self.approot.add(path)
-    
+
+        self.build.add(path,f"approot/{path.split("/")[-1]}")
+    def build_app(self):
+        with open(f"{self.tempfolder}/config.json","w") as file:
+            json.dump(self.config,file)
+        self.build.add(f"{self.tempfolder}/config.json","config.json")
+        self.build.close()
